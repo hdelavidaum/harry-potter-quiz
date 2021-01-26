9a15213078ed97c3c2780e3bc6ca5bae0be4fd2f
Henrique Delavi Daum <henrique.delavi.daum@gmail.com>
2021-01-25 22:20:23 -0300

    fix: meta tags

--- src/components/HeadTags/index.tsx
+++ src/components/HeadTags/index.tsx
@@ -12,15 +12,15 @@ const HeadTags = () => {
             {/* Open Graph / Facebook */}
             <meta property="og:type" content="website" />
             <meta property="og:url" content="https://harry-potter-quiz.hdelavidaum.vercel.app/" />
-            <meta property="og:title" content="HP Quiz - Imersão Alura - React + Next.js" />
-            <meta property="og:description" content="Quiz of Harry Potter's World developed by Henrique Delavi Daum at 'Imersão Alura React + NextJS' and deployed with Vercel" />
+            <meta property="og:title" content="HP Quiz|Imersão Alura|React+Next.js" />
+            <meta property="og:description" content="Developed by Henrique Delavi Daum at 'Imersão Alura React+NextJS'" />
             <meta property="og:image" content="https://raw.githubusercontent.com/hdelavidaum/harry-potter-quiz/main/assets/harry-potter-bg.jpg" />
 
             {/* Twitter */}
             <meta property="twitter:card" content="summary_large_image" />
             <meta property="twitter:url" content="https://harry-potter-quiz.hdelavidaum.vercel.app/" />
-            <meta property="twitter:title" content="HP Quiz - Imersão Alura - React + Next.js" />
-            <meta property="twitter:description" content="Quiz of Harry Potter's World developed by Henrique Delavi Daum at 'Imersão Alura React + NextJS' and deployed with Vercel" />
+            <meta property="twitter:title" content="HP Quiz|Imersão Alura|React+Next.js" />
+            <meta property="twitter:description" content="Developed by Henrique Delavi Daum at 'Imersão Alura React+NextJS'" />
             <meta property="twitter:image" content="https://raw.githubusercontent.com/hdelavidaum/harry-potter-quiz/main/assets/harry-potter-bg.jpg" />
         </Head>
     )
