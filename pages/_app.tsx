c8759739535feef766fc735c68dd0b3f70058a36
Henrique Delavi Daum <henrique.delavi.daum@gmail.com>
2021-01-25 22:11:45 -0300

    meta tags imported at _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 import db from '../db.json';
+import HeadTags from '../src/components/HeadTags'
 
 interface Idb {
     theme:{
@@ -48,6 +49,7 @@ const theme = db.theme;
 export default function App({ Component, pageProps }: any) {
   return (
     <>
+      < HeadTags />
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
